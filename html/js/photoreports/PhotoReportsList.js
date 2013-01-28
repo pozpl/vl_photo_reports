6631b0ba80fa7fc08513b51b526eaf1dc56b4dea
Pavel Pozdniak <pozdpl@gmail.com>
2013-01-28 21:14:45 +1100

    master once again I tried to make this world prettier

--- html/js/photoreports/PhotoReportsList.js
+++ html/js/photoreports/PhotoReportsList.js
@@ -17,7 +17,7 @@ define(["dojo/_base/declare", "dojo/_base/array", "dojo/_base/lang", "dojo/i18n"
 			serviceUrl: "http://localhost:5000/app/get/json/all",
 			// Create a template string for tweets:
 			tweetTemplateString:'<div>'+
-				'<img src="{{poster}}" alt="{{event_name}}" class="photo_report_poster" />'
+				'<img src="{{poster}}" alt="{{event_name}}" class="photo_report_poster"  align="left"/>'
 				+ '<div class="eventInfo">'
 				+'<div class="companyName">{{company_name}}</div>'
 				+'<div class="eventName">{{event_name}}</div>'
