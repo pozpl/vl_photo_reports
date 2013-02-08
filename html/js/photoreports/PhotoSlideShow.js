58806a569e0c6bf9690ea8b5d7f0ab14192255c9
Pavel Pozdniak <pozdpl@gmail.com>
2013-02-08 21:36:12 +1100

    master Make a photo slide gallery on top of a previos carousel widget that I decide is not what I want

---
+++ html/js/photoreports/PhotoSlideShow.js
@@ -0,0 +1,51 @@
+define(["dojo/_base/declare", "dojo/_base/array", "dojo/_base/lang", "dojo/i18n", "dojo/dom-class",
+	"dojo/dom-attr","dojox/mobile/View", "dojo/store/Memory", "dijit/registry"],
+	function(declare, arrayUtil, lang, i18n, domClass, domAttr, View,
+	         Memory, registry) {
+		// Return the declared class!
+		return declare("photoreports.PhotoCarousel", [View], {
+			//Store to get
+			photoReportStore : {},
+
+			// When the widgets have started....
+			startup: function() {
+				// Retain functionality of startup in dojox/mobile/ScrollableView
+				this.inherited(arguments);
+
+
+				dojo.connect(this, "onBeforeTransitionIn", null,
+					function(moveTo, dir, transition, context, method){
+						var photoCarousel = registry.byId("photo_carousel");
+						photoCarousel.setStore(this.photoReportStore);
+						photoCarousel.startup();
+					});
+
+
+//				var photoCarousel = new StoreCarousel({
+//					store: sampleStore,
+//					height: "150px",
+//					navButton: true,
+//					numVisible: 2,
+//					title: "Category"
+//				}, "carousel1");
+//				carousel.startup();
+			},
+
+
+			setPhotoReportStore: function(photosArray) {console.log('set');
+				var preparedImagesArray = new Array();
+				arrayUtil.forEach(photosArray, function(image, imageIndex){
+					preparedImagesArray[imageIndex] = {
+						'src' : 'http://img.vl.ru/i/catalog/' + image.imageFilePathBig,
+						'value' : '',
+						'headerText' : ''
+					};
+
+				}, this);
+				this.photoReportStore = new Memory({data: {
+					'items': preparedImagesArray
+				}});
+			}
+
+		});
+	});
