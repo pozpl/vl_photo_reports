d1f87dcbb9c7f9af99c984ee5e4b9fecfc470e66
Pavel Pozdniak <pozdpl@gmail.com>
2013-03-18 07:40:50 +1100

    master Take out some device specific functions in separate module

---
+++ html/js/photoreports/DeviceSpecificFunctions.js
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pozpl
+ * Date: 18.03.13
+ * Time: 7:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+define(["dojo/_base/declare", "dojo/query"],
+    function(declare, arrayUtil, lang, i18n, domClass, domAttr, ScrollableView, ListItem, DeferredList,
+             ioScript, registry, query, dtl, dtlContext) {
+        // Return the declared class!
+        return declare("photoreports.PhotoReportsList", [ScrollableView], {
+
+
+
+        });
+
+});
