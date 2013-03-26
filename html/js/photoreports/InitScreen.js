105020dbb84675f03ee3eea0022483e8f1807ead
Pavel Pozdniak <pozdpl@gmail.com>
2013-03-26 22:44:51 +1100

    master Hay waka waka? And now Im going closer to perform true history in this mobile application

--- html/js/photoreports/InitScreen.js
+++ html/js/photoreports/InitScreen.js
@@ -5,11 +5,10 @@
  * Time: 21:29
  * To change this template use File | Settings | File Templates.
  */
-define(["dojo/_base/declare", "dojox/mobile/View"],
-    function(declare, arrayUtil, lang, i18n, domClass, domAttr, View, ListItem, DeferredList,
-             ioScript, registry, query, dtl, dtlContext) {
+define(["dojo/_base/declare", "dojox/mobile/View", "dijit/registry"],
+    function(declare, View, registry) {
         // Return the declared class!
-        return declare("photoreports.PhotoReportsList", [View], {
+        return declare("photoreports.InitScreen", [View], {
             // When the widgets have started....
             startup: function() {
                 // Retain functionality of startup in dojox/mobile/ScrollableView
@@ -18,10 +17,10 @@ define(["dojo/_base/declare", "dojox/mobile/View"],
                 var firstScreenHash = "last_photoreports";
                 var windowHash = window.location.hash;
                 if(windowHash.indexOf(firstScreenHash) == -1){
-                    alert(firstScreenHash);
-                    this.performTransition("#" + firstScreenHash);
+                    var lastPhotoReports = registry.byId("last_photoreports");
+                    lastPhotoReports.startup();
+                    this.performTransition("#" + firstScreenHash, 1, 'fade', null);
                 }
-
             }
 
         });
